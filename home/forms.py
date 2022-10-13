c075a2e593eeb8a0b4d9e410a6f087e1ffab2f1a
Adrian E Ponsone <adrianponsone@yahoo.com.ar>
2022-10-13 18:14:38 -0300

    arreglu un  error en el nombre de una variable en una vista

--- home/forms.py
+++ home/forms.py
@@ -4,7 +4,7 @@ from django import forms
 class UserForm(forms.Form):
     name = forms.CharField(max_length=30)
     last_name = forms.CharField(max_length=30)
-    creation_date = forms.DateField(required=False)
+    registration_date = forms.DateField(required=False)
     
     
 class SearchUserForm(forms.Form):
